502798d507e3a7f1ee694859cac4a5be02d1b160
seerosecode <keshalakemusic@gmail.com>
2019-06-07 10:04:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+var element = document.createElement('div')
+document.body.appendChild(element)
+var ul = document.createElement('li')
+
+for (let i = 0; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+
+}
+
+element.appendChild(ul)
+
+// let element =
+// document.querySelector("p#greeting");
+// element.innerHTML = "Hello, DOM!"
+
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAligh = 'left'
